edeac4d5031e292cb1dc405e0dfcf5de12b8edb9
bzzzz <bz@defrag.in>
2013-02-13 00:58:53 +0100

    verbosity.

--- update.sh
+++ update.sh
@@ -11,15 +11,22 @@ PUSH=$2
 CURLOPTS="--silent --user-agent \"\""
 CURL=/usr/bin/curl
 
+echo "Fetching https://clientarea.ramnode.com/privacy.php"
 $CURL $CURLOPTS --output privacy https://clientarea.ramnode.com/privacy.php
+echo "Fetching https://clientarea.ramnode.com/aup.php"
 $CURL $CURLOPTS --output aup https://clientarea.ramnode.com/aup.php
 
 if [ "`git diff`" != "" ]
 then
+    echo "Committing changes."
     git checkout -B "$BRANCH"
     git add privacy aup
     git commit -m "`date`"
     git checkout -
 
-    [ "$PUSH" != "" ] && git push origin "$BRANCH"
+    if [ "$PUSH" != "" ]
+    then
+        echo "Pushing branch $BRANCH to origin."
+        git push origin "$BRANCH"
+    fi
 fi
