bda31a8abd1479df3bbdfbcb20127143ac2f20ae
bzzzz <bz@defrag.in>
2013-02-13 03:09:51 +0100

    change branches before pulling new data. abort on checkout failure.

--- update.sh
+++ update.sh
@@ -11,14 +11,28 @@ PUSH=$2
 CURLOPTS="--silent --user-agent \"\""
 CURL=/usr/bin/curl
 
+[ "`git branch --list \"$BRANCH\"`" = "" ] && git branch "$BRANCH"
+git checkout -q "$BRANCH"
+if [ "`git branch | grep \"*\" | cut -d \" \" -f 2`" != "$BRANCH" ]
+then
+    echo "Could not check out the $BRANCH branch. Aborting."
+    exit
+fi
+
 echo "Fetching https://clientarea.ramnode.com/privacy.php"
 $CURL $CURLOPTS --output privacy https://clientarea.ramnode.com/privacy.php
 echo "Fetching https://clientarea.ramnode.com/aup.php"
 $CURL $CURLOPTS --output aup https://clientarea.ramnode.com/aup.php
 
-git checkout -B "$BRANCH"
-git add privacy aup
-git commit -m "`date`"
+if [ "`git status --porcelain -- privacy aup`" != "" ]
+then
+    git status --porcelain -- privacy aup
+    git add privacy aup
+    git commit -m "`date`"
+else
+    echo "no changes"
+fi
+
 git checkout -
 
 if [ "$PUSH" != "" ]
